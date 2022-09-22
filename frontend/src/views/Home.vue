9b8c2a43c9fa0bd077e5a0d87374a0270cfd530a
vam2004 <vam2004@github.com>
2022-09-22 09:21:29 -0400

    workflow: integrating frontend - files

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>
+      Mamãe linda te amo
+      <img src="https://cdn-icons-png.flaticon.com/512/44/44822.png" />
+    </h1>
+    <h2>Nossos Diferenciais</h2>
+    <ul>
+      <li>taxa de juros baixíssimas, apenas 50% ao dia</li>
+      <li>Funcionários dedicados:</li>
+      <img
+        src="http://beatriziolanda.com/wp-content/uploads/2014/01/dormir-horario-de-trabalho.jpg"
+      />
+      <li>Diferente dos agiotas, queremos apenas seu dinheiro</li>
+    </ul>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+* {
+  margin: 2rem;
+}
+img {
+  width: 25rem;
+}
+</style>
